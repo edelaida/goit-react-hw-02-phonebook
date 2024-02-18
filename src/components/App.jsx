58f7ed6f9ed23e29fb84b4d4e0bbb2a88e8e0250
Levina Tani <edelaida@gmail.com>
2024-02-19 00:00:11 +0200

    homework-start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +9,23 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
     </div>
   );
 };
+
+// import { Statistics } from './Statistics/Statistics.jsx';
+// import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
+// import { Button } from './Button.jsx';
+
+// export class App extends React.Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   onLeaveFeedback = value => {
